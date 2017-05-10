6b8b7f1b715c1bf8f86d43c9b358c11d71d12875
jiaxinhuang <924785452@qq.com>
2017-05-10 19:31:50 +0800

    complete the stackencoder

--- stackedae_exercise/stackedAEPredict.m
+++ stackedae_exercise/stackedAEPredict.m
@@ -23,8 +23,17 @@ stack = params2stack(theta(hiddenSize*numClasses+1:end), netconfig);
 %% ---------- YOUR CODE HERE --------------------------------------
 %  Instructions: Compute pred using theta assuming that the labels start 
 %                from 1.
-
-
+aeNumber = numel(stack);
+m = size(data, 2);
+z = cell(aeNumber+1, 1);
+a = cell(aeNumber+1, 1);
+a{1} = data;
+for i=2:aeNumber+1
+    z{i} = stack{i-1}.w*a{i-1} + repmat(stack{i-1}.b, 1, m);
+    a{i} = sigmoid(z{i});
+end
+theta_x = softmaxTheta * a{aeNumber+1};
+[max_theta_x, pred] = max(theta_x);
 
 
 
